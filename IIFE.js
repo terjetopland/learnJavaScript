94ce660c904c1b8417776bf033afafa2b5e0f2b1
terjetop <terjetop@uia.no>
2023-10-28 09:24:29 +0200

    Added IIFE

---
+++ IIFE.js
@@ -0,0 +1,7 @@
+// IIFE stands for "Immediately Innvoked Function Expression"
+// It means a function that is living in its own scope and is called immediately after created.
+
+(function () {
+    const message = `I am invoked immediately after I was created`;
+    console.log(message);
+})();
