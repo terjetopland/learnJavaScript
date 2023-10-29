0e807eb51ff7fc2f5f63acb6c2a8c3dbb8fc6dc4
terjetop <terjetop@uia.no>
2023-10-29 08:39:17 +0100

    Added factorial calc to recursiveFunction.js

--- recursiveFunction.js
+++ recursiveFunction.js
@@ -1,3 +1,18 @@
+// Typical example of using recursive function would be to make a factorial function.
+const factorialRecursive = (number, originalNumber=0, calculated=1) => {
+    if(originalNumber === 0) {
+        originalNumber = number;
+    }
+    if(number === 1) {
+        console.log(`Factorial of ${originalNumber} is: ${calculated}`);
+        return ;
+    }
+    calculated *= number;
+    console.log(`... iteration and result is: ${calculated}`)
+    return number * factorialRecursive(number-1, originalNumber, calculated);
+} 
+
+
 const recursiveFunc = (stopAfterItteration, count=0) => {
     const stop = 0;
     const max = 100;
@@ -9,5 +24,9 @@ const recursiveFunc = (stopAfterItteration, count=0) => {
     return recursiveFunc(stopAfterItteration-1, count+1)
     
 };
+
+
+
+factorialRecursive(5);
 
 recursiveFunc(20);
