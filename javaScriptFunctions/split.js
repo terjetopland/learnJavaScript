489a973d8592248267d599fa3dc79a523387cee4
terjetop <terjetop@uia.no>
2023-11-05 16:02:40 +0100

    Added input form to submit your name

--- javaScriptFunctions/split.js
+++ javaScriptFunctions/split.js
@@ -1,19 +1,42 @@
 //* Let's try the split method / function
 
 // Make an array either as [] or a string.
-const helloArray = ['h', 'e', 'l', 'l', 'o'];
-const message = 'How "are" you?';
+// const helloArray = ['h', 'e', 'l', 'l', 'o'];
+// const message = 'How "are" you?';
 
-let splitMessageKeep2 = message.split(' ', 2);
-//console.log(splitMessageKeep2); //returns ['How', '"are"']
+//let splitMessageKeep2 = message.split(' ', 2); //returns ['How', '"are"']
+//console.log(splitMessageKeep2); 
 
 // Split with "regular expression" (regex):
-let splitMessageRegEx = message.split(/[.,!,?,"]/);
-//console.log(splitMessageRegEx);
+//let splitMessageRegEx = message.split(/[.,!,?,"]/); // returns ['How', 'are', 'you']
+//console.log(splitMessageRegEx); 
 
 // Use split method and asign each substring to a variable:
-let userName = 'Terje Topland';
+//let userName = 'Terje Topland';
 // split at every whitespace and asssign each element to a new variable.
-let [firstName, lastname] = userName.split(' ');
+//let [firstName, lastName] = userName.split(' ');
 //console.log(firstName); // output: Terje
+
+const splitUserName = (inputUserName) => {
+    let [firstName, lastName] = inputUserName.split(' ');
+
+    return {
+        firstName: firstName,
+        lastName: lastName
+    };
+    // With this return u return and object. In that way you can access which of them u want.
+}
+
+export const formUserNameHandler = (event) => {
+    event.preventDefault();
+    const inputUserName = document.getElementById('userName').value;
+
+    const firstName = splitUserName(inputUserName).firstName;
+
+    const welcomeDiv = document.getElementById('welcomeUser');
+    welcomeDiv.textContent = `Welcome ${firstName}`;
+
+    document.getElementById('userName').value = '';
+}
+
 
--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 
 import { user } from "./conceptsJS/immutableObject.js";
 import { changeUser } from "./conceptsJS/immutableObject.js";
+import { formUserNameHandler } from "./javaScriptFunctions/split.js";
 
+document.getElementById('formUserName').addEventListener('submit', (eventObject) => {
+    eventObject.preventDefault(); // Prevents refreshing...
+    formUserNameHandler(eventObject);
+} )
 
-// factorialRecursive(6);
-// console.log(user);
-// console.log(user);
-// const newUser = user;
-// changeUser(newUser, 'user');
-// console.log(newUser);
