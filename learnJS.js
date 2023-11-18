34e60a8f6d7dcf3076b5895c7f88bf189ab79c6f
terjetop <terjetop@uia.no>
2023-11-18 13:19:11 +0100

    Made the hamburger a button instead

--- learnJS.js
+++
@@ -1,6 +0,0 @@
-import { formUserNameHandler } from "./javaScriptFunctions/split.js";
-
-document.getElementById('formUserName').addEventListener('submit', (eventObject) => {
-    eventObject.preventDefault(); // Prevents refreshing...
-    formUserNameHandler(eventObject);
-} )
--- main.js
+++ main.js
@@ -1,14 +1,27 @@
-document.addEventListener('DOMContentLoaded', function () {
+import { formUserNameHandler } from "./javaScriptFunctions/split.js";
+
+const formUserName = document.getElementById('formUserName');
+
+if (formUserName) {
+    addEventListener('submit', (eventObject) => {
+        eventObject.preventDefault(); // Prevents refreshing...
+        formUserNameHandler(eventObject);
+    })
+}
+
+document.addEventListener('DOMContentLoaded', async function () {
     var menuButton = document.getElementById('menuButton');
     var menu = document.querySelector('.navBar .menu');
 
-    menuButton.addEventListener('click', function () {
-        // Toggle the menu visibility by changing the right style
-        if (menu.style.right === '0%') {
-            menu.style.right = '-100%';
-        } else {
-            menu.style.right = '0%';
-        }
-    });
+    if (menuButton) {
+        menuButton.addEventListener('click', async function () {
+            // Toggle the menu visibility by changing the right style
+            if (menu.style.right === '0%') {
+                menu.style.right = '-100%';
+            } else {
+                menu.style.right = '0%';
+            }
+        });
+    }
 });
 
