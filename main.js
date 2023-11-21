ffaee85e76c072dffd848558afaeca2f9e423d36
terjetop <terjetop@uia.no>
2023-11-21 16:02:22 +0100

    Added js make more interactions with the flipping cards

--- main.js
+++ main.js
@@ -42,3 +42,49 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
+// in learnCSS add card if card is clicked
+document.addEventListener('DOMContentLoaded', () => {
+    let firstCard = document.getElementById('firstCard')
+    let secondCard = document.getElementById('secondCard');
+    let thirdCard = document.getElementById('thirdCard');
+
+    if (secondCard) {
+        firstCard.addEventListener('click', () => {
+            if (secondCard.style.opacity < '1') {
+                secondCard.style.cursor = 'pointer';
+                secondCard.style.opacity = '1';
+                firstCard.style.opacity = '0';
+            }
+        });
+        secondCard.addEventListener('click', () => {
+            if (secondCard.style.opacity > '0') {
+                secondCard.style.cursor = 'default';
+                secondCard.style.opacity = '0';
+            }
+        });
+    }
+
+    if (thirdCard) {
+        secondCard.addEventListener('click', () => {
+            if (thirdCard.style.opacity < '1') {
+                thirdCard.style.cursor = 'pointer';
+                thirdCard.style.opacity = '1';
+            }
+        });
+        thirdCard.addEventListener('click', () => {
+            if (thirdCard.style.opacity > '0') {
+                thirdCard.style.cursor = 'default';
+                thirdCard.style.opacity = '0';
+                firstCard.style.opacity = '1';
+
+                let changeTextFirstTime = false;
+                let frontText = firstCard.querySelector('.flipCardFront > div');
+                if (frontText && !changeTextFirstTime) {
+                    frontText.textContent = 'You found me again';
+                    changeTextFirstTime = true;
+                }
+            }
+        });
+    }
+});
+
